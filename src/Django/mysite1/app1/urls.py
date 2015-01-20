2104e608e5758161bad06b29dfd99dcade789257
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-01-20 11:45:03 -0500

    Views tjat do something

--- src/Django/mysite1/app1/urls.py
+++ src/Django/mysite1/app1/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import patterns, url
 from app1 import views
 
 urlpatterns = patterns('',
+    # /app1
     url(r'^$', views.index, name='index'),
+    # /polls/5/
+    url(r'^(?P<name_id>\d+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<name_id>\d+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<name_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- src/Django/mysite1/app1/views.py
+++ src/Django/mysite1/app1/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from app1.models import Name
 
 
 def index(request):
-    return HttpResponse("Welcome to the Ricardo's directory")
+    latest_name_list = Name.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('app1/index.html')
+    context = RequestContext(request, {
+        'latest_name_list': latest_name_list,
+    })
+    return HttpResponse(template.render(context))
+
 
 # Create your views here.
+def detail(request, name_id):
+    return HttpResponse("You're looking at name %s." % name_id)
+
+def results(request, name_id):
+    response = "You're looking at the results of name %s."
+    return HttpResponse(response % name_id)
+
+def vote(request, name_id):
+    return HttpResponse("You're voting on question %s." % name_id)
