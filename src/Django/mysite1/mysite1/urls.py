e719d5a53b45113805294f34d57546bf0790b299
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-01-20 16:50:59 -0500

    Redirect root / to app1

--- src/Django/mysite1/mysite1/urls.py
+++ src/Django/mysite1/mysite1/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite1.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/app1/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^app1/', include('app1.urls', namespace="app1")),
 )
