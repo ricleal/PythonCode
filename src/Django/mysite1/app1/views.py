4451fc6fb681f90698aa86dd80e38afc116a3145
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-01-20 10:37:36 -0500

    Simple view

--- src/Django/mysite1/app1/views.py
+++ src/Django/mysite1/app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to the Ricardo's directory")
 
 # Create your views here.
