a947a652a81c25573467886b67b76217ce776e50
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-01-20 11:49:53 -0500

    View and template for detail

--- src/Django/mysite1/app1/views.py
+++ src/Django/mysite1/app1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from django.template import RequestContext, loader
 
 from app1.models import Name
@@ -13,7 +13,11 @@ def index(request):
 
 # Create your views here.
 def detail(request, name_id):
-    return HttpResponse("You're looking at name %s." % name_id)
+    try:
+        name = Name.objects.get(pk=name_id)
+    except Name.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'app1/detail.html', {'name': name})
 
 def results(request, name_id):
     response = "You're looking at the results of name %s."
