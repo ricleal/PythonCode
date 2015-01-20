7445393142aad29cd3143e0512184072239ef84a
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-01-20 12:00:29 -0500

    Shortcut to 404

--- src/Django/mysite1/app1/views.py
+++ src/Django/mysite1/app1/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from app1.models import Name
@@ -13,10 +13,7 @@ def index(request):
 
 # Create your views here.
 def detail(request, name_id):
-    try:
-        name = Name.objects.get(pk=name_id)
-    except Name.DoesNotExist:
-        raise Http404("Question does not exist")
+    name = get_object_or_404(Name, pk=name_id)
     return render(request, 'app1/detail.html', {'name': name})
 
 def results(request, name_id):
