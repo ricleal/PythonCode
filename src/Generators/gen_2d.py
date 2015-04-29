e9a742aee7c5b615e30cfe01c22d897796b8e281
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-04-29 16:41:59 -0400

    Write format to export to Java script

--- src/Generators/gen_2d.py
+++ src/Generators/gen_2d.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def make_it_nan (array, distance):
+    '''
+    For a 2D array makes NaN all that is
+    higher than the distance
+    '''
     for i, row in enumerate(array):
         for j, value in enumerate(row):
             if dist[i,j] > distance:
@@ -10,22 +14,29 @@ def make_it_nan (array, distance):
             
 
 def dump_as_csv(x,y,z):
-    # Make sparse false!
+    """
+    Dump the contents of x,y,z to CSV and txt files
+    For Serena to use the txt files into javascript
+    """
     
     xx, yy = np.meshgrid(x, y, sparse=False)
     a = zip(xx.flatten(),yy.flatten(),z.flatten())
     ar = np.array(a)
     np.savetxt("/tmp/array.csv", ar, fmt='%.2e', delimiter=',', newline='\n', header='x,y,z')
     with open("/tmp/array.txt","w") as f:
+        f.write("[")
         for row in ar:
-            f.write("[%s]\n" % ', '.join(str(x) for x in row) )
-    
+            f.write("[%s],\n" % ', '.join(str(x) for x in row) )
+        f.write("]")
+    ## ugly DRY
     a = zip(xx.flatten(),yy.flatten(),z.flatten(), np.sqrt(z.flatten()) )
     ar = np.array(a)
     np.savetxt("/tmp/array_err.csv", ar, fmt='%.2e', delimiter=',', newline='\n', header='x,y,z,err')
     with open("/tmp/array_err.txt","w") as f:
+        f.write("[")
         for row in ar:
-            f.write("[%s]\n" % ', '.join(str(x) for x in row) )
+            f.write("[%s],\n" % ', '.join(str(x) for x in row) )
+        f.write("]")
     
 
 x = np.arange(-5, 5, 0.1)
