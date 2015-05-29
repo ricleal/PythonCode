ff9f0cdf80c20172390287b61bf3e31650b13cbb
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-05-29 18:17:32 -0400

    Playing with least squares

--- src/Fitting/optimise1.py
+++ src/Fitting/optimise1.py
@@ -1,33 +1,49 @@
-from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq as optimizer
 
-x = arange(0, 6e-2, 6e-2/50)
-A, k, theta = 10, 1.0/3e-2, pi/6
+def rmsd(a,b):
+    return np.sqrt((a-b)**2)
 
-y_true = A*sin(2*pi*k*x+theta)
-y_meas = y_true + 2*random.randn(len(x))
+def func(x,A,k,theta):
+    return A*np.sin(2*np.pi*k*x+theta)
+
+x = np.arange(0, np.pi, np.pi/200)
+
+A = 20
+k = np.pi/12
+theta = np.pi/6
+
+y_true = func(x,A,k,theta)
+y_meas = y_true + 2*np.random.randn(len(x))
 
 def residuals(p, y, x):
     A,k,theta = p
-    err = y-A*sin(2*pi*k*x+theta)
+    err = y-func(x,A,k,theta)
     return err
 
 def peval(x, p):
-    return p[0]*sin(2*pi*p[1]*x+p[2])
+    A,k,theta = p
+    return func(x,A,k,theta)
 
-p0 = [8, 1/2.3e-2, pi/3]
-print array(p0)
+p0 = [np.max(y_meas), np.pi/11, np.pi/3]
+print 'Guess:\t',np.array(p0)
 # [  8.      43.4783   1.0472]
 
-from scipy.optimize import leastsq
-plsq = leastsq(residuals, p0, args=(y_meas, x))
-print plsq[0]
+
+plsq = optimizer(residuals, p0, args=(y_meas, x))
+print 'Found:\t',plsq[0]
 # [ 10.9437  33.3605   0.5834]
 
-print array([A, k, theta])
+print 'Theor:\t',np.array([A, k, theta])
 # [ 10.      33.3333   0.5236]
 
-import matplotlib.pyplot as plt
-plt.plot(x,peval(x,plsq[0]),x,y_meas,'o',x,y_true)
+print 'RMSD :\t',rmsd(plsq[0], np.array([A, k, theta]))
+
+plt.plot(x,peval(x,plsq[0]), label='Fit' )
+plt.plot(x,y_meas,'x', label='Measured')
+plt.plot(x,y_true , label='Theoretical')
+
 plt.title('Least-squares fit to noisy data')
-plt.legend(['Fit', 'Noisy', 'True'])
+plt.legend()
 plt.show()
