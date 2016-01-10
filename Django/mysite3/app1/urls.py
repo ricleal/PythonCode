ae8fccb963a0624a8a02cf9e92964297f0592e0f
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-01-10 09:50:36 -0500

    Removed site3

--- Django/mysite3/app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from . import views
-
-urlpatterns = [
-    # /app1
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^browser/$', views.dirlist, name='browser'),
-]
