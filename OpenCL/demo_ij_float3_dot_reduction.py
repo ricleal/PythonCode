2b3c546ae197214352d03a704ee7cee5df169874
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-06-11 20:50:29 -0400

    pyopencl kernel nested kernels

---
+++ OpenCL/demo_ij_float3_dot_reduction.py
@@ -0,0 +1,57 @@
+from pyopencl import clrandom
+import numpy as np
+import pyopencl as cl
+
+"""
+Dot product element wise of 2 vector of 3d coordinates
+Using Float3
+
+"""
+
+source = """
+__kernel void gpu_mul(__global const float *a, __global const float *b, float c) {
+
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+
+    int size_a = get_global_size(0);
+    int size_b = get_global_size(1);
+
+    float3 vec_a = vload3(i, a);
+    float3 vec_b = vload3(j, b);
+
+    int idx = i*size_b + j;
+    //c[idx] = size_a + size_b;
+
+    float res = dot(vec_a, vec_b);
+    c = res;
+
+} // execute over n "work items"
+"""
+
+ctx = cl.create_some_context()
+queue = cl.CommandQueue(ctx)
+
+# create some data array to give as input to Kernel and get output
+SIZE = 4
+a_np = np.arange(SIZE*3).reshape(SIZE,3).astype(np.float32)
+b_np = np.arange(SIZE*3, SIZE*3+SIZE*3).reshape(SIZE,3).astype(np.float32)
+
+# create the buffers to hold the values of the input
+a_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,hostbuf=a_np)
+b_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,hostbuf=b_np)
+
+c = np.float(0)
+#Compilation
+prg = cl.Program(ctx, source).build()
+
+# Kernel is now launched
+launch = prg.gpu_mul(queue, (SIZE,SIZE), None, a_buf, b_buf, c)
+# wait till the process completes
+launch.wait()
+
+
+# print the output
+print "a:\n", a_np
+print "b:\n", b_np
+print "c:\n", c
--- OpenCL/pyopencl_kernel_1.py
+++ OpenCL/pyopencl_kernel_1.py
@@ -7,33 +7,67 @@ from __future__ import print_function
 import numpy as np
 import pyopencl as cl
 import pyopencl.array
+import time
 from pyopencl.elementwise import ElementwiseKernel
 from pyopencl.reduction import ReductionKernel
 
-n = 10
-a_np = np.ones(n).astype(np.float32)*2
-b_np = np.ones(n).astype(np.float32)*2
+'''
+Run as:
+PYOPENCL_CTX='0:2' PYOPENCL_COMPILER_OUTPUT=1 python pyopencl_kernel_1.py
+
+Bit wise operation on Vector of 4d coordinates (4th is zero)
+followed by a Reduction
+Does not work on macos....
+
+'''
+# float4 is better than float3! Append 0 as w
+n = 10000000 * 3
+
+z = np.zeros((n,1), dtype=np.float32)
+a_np = np.append(np.random.random((n,3)).astype(np.float32), z, axis=1)
+b_np = np.append(np.random.random((n,3)).astype(np.float32), z, axis=1)
+
 
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 a_g = cl.array.to_device(queue, a_np)
 b_g = cl.array.to_device(queue, b_np)
-res_g = cl.array.empty_like(a_g)
+
+res_g = cl.array.empty(queue, shape=(n,), dtype=np.float32)
+
+start = time.time()
 
 elem_wise_krnl = ElementwiseKernel(ctx,
-    "float *a_g, float *b_g, float *res_g",
-    "res_g[i] = a_g[i] * b_g[i]",
-    "lin_comb"
+    arguments="float4 *a_g, float4 *b_g, float *res_g",
+    operation="res_g[i] = dot(a_g[i],b_g[i])",
+    name="elem_wise_krnl"
 )
 elem_wise_event = elem_wise_krnl(a_g, b_g, res_g)
 
+elem_wise_time = time.time()
+
 # np.set_printoptions(precision=2)
 # print(res_g.get())
+# print(res_g.get().shape)
+# print(res_g.get()[:10])
 
-reduction_krnl = ReductionKernel(ctx, np.float32, neutral="0",
-        reduce_expr="a+b", map_expr="x[i]",
-        arguments="__global float *x")
+reduction_krnl = ReductionKernel(ctx,
+    dtype_out=np.float32,
+    neutral="0",
+    reduce_expr="a+b",
+    map_expr="x[i]",
+    arguments="__global float *x",
+    name="reduction_krnl",
+)
+
 res_reduction = reduction_krnl(res_g, queue=queue, wait_for=[elem_wise_event])
+
+reduction_time = time.time()
+
+print("elem_wise_time: {}".format(elem_wise_time-start))
+print("reduction_time: {}".format(reduction_time-elem_wise_time))
+print("Total: {}".format(reduction_time-start))
+#print(res_reduction)
 
 print(res_reduction.get())
