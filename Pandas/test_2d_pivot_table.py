43b09169e29f18a3c33527a36d94db4e4905d5f5
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-08-28 09:30:54 -0400

    Converts 2D mumpy array indo pandas Dataframe and back into a 2D array

---
+++ Pandas/test_2d_pivot_table.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+'''
+
+Converts 2D mumpy array indo pandas Dataframe and back into a 2D array
+
+Matrix ij convention
+i is Row
+j is column
+'''
+
+n_rows, n_cols = shape = (2,5) # pos 0 rows, pos 1 columns
+data = np.arange(10).reshape(n_rows, n_cols)
+print("---- Data ----")
+print(data)
+assert(data[1,0] == 5)
+
+rows_v, cols_v = np.meshgrid(range(n_rows), range(n_cols), indexing='ij')
+d = {'i' : rows_v.ravel(), # i = rows
+     'j' : cols_v.ravel(), # j = coluns
+     'value' : data.ravel(),
+}
+
+df = pd.DataFrame(d)
+pivot = df.pivot(index='i', columns='j', values='value')
+print("---- DataFrame ----")
+print(pivot)
+assert(pivot.loc[1][0] == 5)
+
+print("---- DataFrame as 2D array ----")
+print(pivot.values)
+assert(data[1,0] == pivot.values[1,0])
