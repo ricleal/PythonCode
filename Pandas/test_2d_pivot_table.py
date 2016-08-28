e7ccd341cb87e2fbb8c228d59c32c31f5a55731f
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-08-28 10:11:58 -0400

    Converts 2D mumpy more options

--- Pandas/test_2d_pivot_table.py
+++ Pandas/test_2d_pivot_table.py
@@ -3,31 +3,38 @@ import numpy as np
 
 '''
 
-Converts 2D mumpy array indo pandas Dataframe and back into a 2D array
+Converts 2D mumpy array into pandas Dataframe and back into a 2D array
 
 Matrix ij convention
 i is Row
 j is column
 '''
 
-n_rows, n_cols = shape = (2,5) # pos 0 rows, pos 1 columns
+n_rows, n_cols = shape = (2, 5)  # pos 0 rows, pos 1 columns
 data = np.arange(10).reshape(n_rows, n_cols)
 print("---- Data ----")
 print(data)
-assert(data[1,0] == 5)
+assert(data[1, 0] == 5)
 
 rows_v, cols_v = np.meshgrid(range(n_rows), range(n_cols), indexing='ij')
-d = {'i' : rows_v.ravel(), # i = rows
-     'j' : cols_v.ravel(), # j = coluns
-     'value' : data.ravel(),
-}
+d = {'i': rows_v.ravel(),  # i = rows
+     'j': cols_v.ravel(),  # j = coluns
+     'value': data.ravel(),
+     }
 
 df = pd.DataFrame(d)
-pivot = df.pivot(index='i', columns='j', values='value')
 print("---- DataFrame ----")
+print(df)
+
+pivot = df.pivot(index='i', columns='j', values='value')
+print("---- DataFrame Pivot ----")
 print(pivot)
 assert(pivot.loc[1][0] == 5)
 
-print("---- DataFrame as 2D array ----")
+print("---- DataFrame Pivot as 2D array ----")
 print(pivot.values)
-assert(data[1,0] == pivot.values[1,0])
+assert(data[1, 0] == pivot.values[1, 0])
+
+print("---- DataFrame as 2D array----")
+df = df.set_index(['i', 'j'])
+assert(df.loc[1, 0].value == 5)
