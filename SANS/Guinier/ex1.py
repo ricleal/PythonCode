ad3166144bd6cec50896051a75df08332da5a39d
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-08-15 13:27:59 -0400

    Example of a Guinier plot

--- SANS/Guinier/ex1.py
+++ SANS/Guinier/ex1.py
@@ -1,38 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt(
-    "Si_8m12A_abs_1.txt",
-    skip_header=2,
-    delimiter=",",
-)
+filenames = [
+"/HFIR/CG2/IPTS-0828/exp158/Shared/CG2_exp158_scan0039_0001_Iq.txt",
+"/HFIR/CG2/IPTS-0828/exp184/Shared/Reduced/CG2_exp184_scan0022_0001_Iq.txt",
+"/HFIR/CG2/IPTS-0828/exp184/Shared/Reduced/CG2_exp184_scan0029_0001_Iq.txt",
+"/HFIR/CG2/IPTS-0828/exp184/Shared/Reduced/CG2_exp184_scan0023_0001_Iq.txt",
+"/HFIR/CG2/IPTS-0828/exp184/Shared/Reduced_ric/CG2_exp184_scan0022_0001_Iq.txt",
+]
 
-# let's just get the interval: 20-30 first points
-x = data[20:30, 0]
-y = data[20:30, 1]
+for f in filenames:
+    data = np.genfromtxt(
+        #"Si_8m12A_abs_1.txt",
+        f,
+        skip_header=2,
+        #delimiter=",",
+    )
 
-plt.figure("Raw data")
-plt.plot(x, y)
-
-
-# I(Q) = I0 * exp(-Q^2*Rg^2 / 3)
-# log(I(Q)) = log(I0) + (-Q^2*Rg^2 / 3) <=>
-# log(I(Q)) = log(I0) - Rg^2/3 * Q^2
-# => Rg = np.sqrt( 3 * b )
-
-x_guinier = np.square(x)
-y_guinier = np.log(y)
-
-# fit data with a polynomial of degree 1: ax+b=0
-# p = [a, b]
-p = np.polyfit(x_guinier, y_guinier, 1)
-y_fitted = np.polyval(p, x_guinier)
+    # let's just get the interval: 20-30 first points
+    # x = data[20:30, 0]
+    # y = data[20:30, 1]
+    
+    print(f)
+    print(data)
+    x = data[:, 0]
+    y = data[:, 1]
 
 
-plt.figure("Guinier plot")
-plt.plot(x_guinier, y_guinier, 'o', label="raw")
-plt.plot(x_guinier, y_fitted, label="fitted")
+    plt.figure("Raw data")
+    plt.plot(x, y)
 
-print("Rg = {}".format(np.sqrt(3 * p[1])))
 
-plt.show()
+    # I(Q) = I0 * exp(-Q^2*Rg^2 / 3)
+    # log(I(Q)) = log(I0) + (-Q^2*Rg^2 / 3) <=>
+    # log(I(Q)) = log(I0) - Rg^2/3 * Q^2
+    # => Rg = np.sqrt( 3 * b )
+
+    x_guinier = np.square(x)
+    y_guinier = np.log(y)
+
+    # fit data with a polynomial of degree 1: ax+b=0
+    # p = [a, b]
+    p = np.polyfit(x_guinier, y_guinier, 1)
+    y_fitted = np.polyval(p, x_guinier)
+
+
+    plt.figure("Guinier plot")
+    plt.plot(x_guinier, y_guinier, 'o', label="raw")
+    plt.plot(x_guinier, y_fitted, label="fitted")
+
+    print("Rg = {}".format(np.sqrt(3 * p[1])))
+
+    plt.show()
