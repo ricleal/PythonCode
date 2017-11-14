94a91d9c71e1cb98143aba9c9df600485db8ad0e
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-11-14 08:38:00 -0500

    python 3 compliant

--- LDAP/test1.py
+++ LDAP/test1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import ldap
 import getpass
 import sys
@@ -14,25 +16,25 @@ url='ldaps://ldap-vip.sns.gov/'
 username='uid=%s,ou=Users,dc=sns,dc=ornl,dc=gov'
 #cert = '/etc/ssl/certs/ca-certificates.crt'
 
-print "Username?"
+print("Username: ", end='')
 user = sys.stdin.readline()
 username = username%user
 pwd = getpass.getpass()
 
 
 try:
-    print "Contacting LDAP...."
+    print("Contacting LDAP....")
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
-    ldapmodule_trace_level = 2
+    ldapmodule_trace_level = 0
     ldapmodule_trace_file = sys.stderr
 
     l = ldap.initialize(url,
                         trace_level=ldapmodule_trace_level,
                         trace_file=ldapmodule_trace_file)
 
-    l.simple_bind_s(username,pwd)
+    l.simple_bind_s(username, pwd)
     l.unbind()
-    print "*"*30, "\nIf I get here then it worked!\n","*"*30
+    print("*"*30, "\nIf I get here then it worked!")
 except ldap.LDAPError, e:
-    print e
+    print(e)
