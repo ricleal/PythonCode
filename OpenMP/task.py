935f6b836a5baf92b2e456cc715532c6e293ccaa
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-05-16 16:21:28 -0400

    Added openmp time

--- OpenMP/task.py
+++ OpenMP/task.py
@@ -1,7 +1,8 @@
 import ctypes as ct
 import numpy as np
 from numpy.ctypeslib import as_ctypes
-
+from timeit import default_timer as timer
+import datetime
 import os
 import sys
 
@@ -14,25 +15,24 @@ def set_N_threads(nthreads):
 
 
 def do_some_task():
-    SIZE = 100
+    SIZE = 1000
+    np.random.seed(0)
 
     input_array = 100 * np.random.random(SIZE)
-    print("Input Max={} Min={} Shape={}".format(
-        input_array.max(), input_array.min(), input_array.shape))
     input_array = np.round(input_array).astype(np.int64)
-    print("Input Max={} Min={} Shape={}".format(
-        input_array.max(), input_array.min(), input_array.shape))
     output_array = np.empty_like(input_array)
 
     print("Starting openmp part...")
+    start = timer()
     lib.do_some_omp_task(as_ctypes(input_array),
                          as_ctypes(output_array),
                          ct.c_size_t(input_array.size))
-    print("End!")
+    end = timer()
+    print("End... Elapsed time: {:0>8}".format(datetime.timedelta(seconds=(end - start))))
     return output_array
 
 
 if __name__ == "__main__":
-    set_N_threads(56)
+    set_N_threads(32)
     out = do_some_task()
     print("Out Max={} Min={} Shape={}".format(out.max(), out.min(), out.shape))
