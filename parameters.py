53af0b60bd9c8986da43dba977161c939a8851ed
Ricardo Ferraz Leal <ricleal@gmail.com>
2018-10-01 11:07:59 -0400

    Commit stuff from macbook

--- parameters.py
+++ parameters.py
@@ -1,17 +1,27 @@
 
 def func(a, b, c):
-    print(a, b, c)
+    return (a, b, c)
 
+def test_func():
 
-func(1, 2, 3)
-func(**dict(a=4, b=5, c=6))
-func(a=7, **dict(b=8, c=9))
+    assert func(1, 2, 3) == (1, 2, 3)
+    assert func(**dict(a=4, b=5, c=6)) == (4, 5, 6)
+    assert func(a=7, **dict(b=8, c=9)) == (7, 8, 9)
 
+    with open('/tmp/params.txt', 'w') as f:
+        f.write("a=10\nb=11\nc=12\n")
 
-with open('/tmp/params.txt', 'w') as f:
-    f.write("a=10\nb=11\nc=12\n")
+    d = {}
+    with open('/tmp/params.txt') as f:
+        d.update(dict(item.strip().split("=") for item in f))
+    assert func(**d) == ('10', '11', '12')
 
-d = {}
-with open('/tmp/params.txt') as f:
-    d.update(dict(item.strip().split("=") for item in f))
-func(**d)
+def func2(filename=None, a=None, b=None, c=None):
+    print(locals())
+    return (a, b, c)
+
+def test_func2():
+    func2()
+
+if __name__ == "__main__":
+    test_func2()
