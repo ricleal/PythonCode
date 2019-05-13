f6678dae57cde36571e2648e949add010142deab
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-05-13 11:19:02 -0400

    pytest added setup and tear down

--- PyTest/test_serial_and_parallel.py
+++ PyTest/test_serial_and_parallel.py
@@ -1,4 +1,4 @@
-# pylint: disable=redefined-outer-name, pointless-string-statement
+# pylint: disable=redefined-outer-name, pointless-string-statement, unused-argument
 
 import pytest
 
@@ -10,6 +10,22 @@ pytest-3 -vs test_serial_and_parallel.py::test_parallel
 '''
 
 
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+    '''
+    By declaring your fixture with autouse=True, it will be automatically
+    invoked for each test function defined in the same module.
+    '''
+    # Code that will run before your test, for example:
+    print("\n** Before the test!")
+
+    # A test function will be run at this point
+    yield
+
+    # Code that will run after your test, for example:
+    print("\n** After the test!")
+
+
 @pytest.fixture
 def data():
     return range(20)
@@ -19,16 +35,16 @@ def square(x):
     return x**2
 
 
-def test_serial(data):
+def test_serial(data, setup_and_teardown):
     res = [square(d) for d in data]
-    assert res == [0,   1,   4,   9,  16,  25,  36,  49,  64,  81, 100, 121,
-                   144, 169, 196, 225, 256, 289, 324, 361]
+    assert res == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81,
+                   100, 121, 144, 169, 196, 225, 256, 289, 324, 361]
 
 
-def test_parallel(data):
+def test_parallel(data, setup_and_teardown):
     import multiprocessing
     with multiprocessing.Pool(processes=2) as pool:
         res = pool.map(square, data)
 
-    assert res == [0,   1,   4,   9,  16,  25,  36,  49,  64,  81, 100, 121,
-                   144, 169, 196, 225, 256, 289, 324, 361]
+    assert res == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81,
+                   100, 121, 144, 169, 196, 225, 256, 289, 324, 361]
