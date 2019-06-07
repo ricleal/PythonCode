3b3b3e845135b7f0c9d109fe334e181735900314
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-06-07 15:13:26 -0400

    Added errors to the wedge integration

--- SANS/sector_averages.py
+++ SANS/sector_averages.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+
+r'''
+find . -iname "*.dat" -exec  python angle_averages.py {} \;
+'''
+
 import sys
 
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
 import scipy.stats as stats
-
-
-'''
-find . -iname "*.dat" -exec  python angle_averages.py {} \;
-'''
 
 
 def get_data(file_name):
@@ -25,58 +25,62 @@ def get_data(file_name):
     return qx, qy, i, err_i
 
 
-def angle_calculation(data_x, data_y, data_z):
+def angle_calculation(data_x, data_y):
     angle = np.arctan2(data_y, data_x)
-    return data_x, data_y, angle
+    return angle
 
 
-def wedge_calculation(data_x, data_y, data_z, angle, phi_0=0, phi_apperture=30):
+def wedge_calculation(data_x, data_y, data_z, error_z, angle, phi_0=0, phi_aperture=30):
     '''
 
     '''
     # Let's work in radians
     phi_0 = np.deg2rad(phi_0)
-    phi_apperture = np.deg2rad(phi_apperture)
+    phi_aperture = np.deg2rad(phi_aperture)
 
-    phi_apperture_min = phi_0 - phi_apperture/2
-    phi_apperture_max = phi_0 + phi_apperture/2
+    phi_aperture_min = phi_0 - phi_aperture/2
+    phi_aperture_max = phi_0 + phi_aperture/2
     # oposite 180 degrees apart
-    phi_apperture_min_pi = phi_apperture_min + np.pi
-    phi_apperture_max_pi = phi_apperture_max + np.pi
-    # if phi_apperture_max_pi > np.pi:
-    #     phi_apperture_max_pi = - phi_apperture_max_pi % np.pi
+    phi_aperture_min_pi = phi_aperture_min + np.pi
+    phi_aperture_max_pi = phi_aperture_max + np.pi
+    # if phi_aperture_max_pi > np.pi:
+    #     phi_aperture_max_pi = - phi_aperture_max_pi % np.pi
 
-    print("Apertures:", phi_apperture_min, phi_apperture_max,
-          phi_apperture_min_pi, phi_apperture_max_pi)
+    print("Apertures:", phi_aperture_min, phi_aperture_max,
+          phi_aperture_min_pi, phi_aperture_max_pi)
     print("Angles", angle.min(), angle.max())
     print("data_y", data_y.min(), data_y.max())
 
-    condition1 = (angle > phi_apperture_min) & (angle < phi_apperture_max)
-    # condition = ((angle > phi_apperture_min) & (angle < phi_apperture_max)) \
-    #     | ((angle > phi_apperture_min_pi) & (angle < phi_apperture_max_pi))
+    condition1 = (angle > phi_aperture_min) & (angle < phi_aperture_max)
+    # make angle > np.pi varying between np.pi and 2*np.pi, rather than the
+    # initial -np.pi to np.pi 
+    angle[angle < 0] = 2*np.pi + angle[angle < 0]
+    condition2 = (angle > phi_aperture_min_pi) & (angle < phi_aperture_max_pi)
 
-    # make angle > np.pi varying between np.pi and 2*np.pi
-    angle[angle < 0] = 2*np.pi + angle[angle < 0] 
-    condition2 = (angle > phi_apperture_min_pi) & (angle < phi_apperture_max_pi)
-    
     condition = condition1 | condition2
 
     data_x = data_x[condition]
     data_y = data_y[condition]
     data_z = data_z[condition]
-    return data_x, data_y, data_z
+    error_z = error_z[condition]
+    return data_x, data_y, data_z, error_z
 
 
-def wedge_integration(x, y, z, bins=100, statistic='mean'):
+def wedge_integration(x, y, z, err_z, bins=100, statistic='mean'):
 
     q_bin_centers = np.sqrt(np.square(x) + np.square(y))
 
-    intensity_statistic, q_bin_edges, q_binnumber = stats.binned_statistic(
+    intensity_statistic, q_bin_edges, _ = stats.binned_statistic(
         q_bin_centers, z, statistic=statistic, bins=bins)
     
+    sigma_statistic, q_bin_edges, _ = stats.binned_statistic(
+        q_bin_centers, err_z,
+        statistic=lambda array_1d: np.sqrt(
+            np.sum(np.square(array_1d))) / len(array_1d), bins=bins)
+
     q_bin_centers = (q_bin_edges[1:] + q_bin_edges[:-1]) / 2.0
 
-    return q_bin_centers, intensity_statistic
+    return q_bin_centers, intensity_statistic, sigma_statistic
 
 
 def sector_average(data_x, data_y, data_z, n_bins_angle=100, n_bins_radius=50):
@@ -94,7 +98,7 @@ def sector_average(data_x, data_y, data_z, n_bins_angle=100, n_bins_radius=50):
     # normalize data to 1
     #data_z = (data_z - data_z.min()) / (data_z.max() - data_z.min())
 
-    H, xedges, yedges, binnumber = stats.binned_statistic_2d(
+    H, xedges, yedges, _ = stats.binned_statistic_2d(
         angle, radius, data_z, bins=[
             n_bins_angle, n_bins_radius], statistic='mean')
 
@@ -108,7 +112,7 @@ def sector_average(data_x, data_y, data_z, n_bins_angle=100, n_bins_radius=50):
 
 
 def do_the_job(file_name):
-    qx, qy, i, _ = get_data(file_name)
+    qx, qy, i, err_i = get_data(file_name)
 
     # QxQy: 1D -> 2D
     shape_qx = len(np.unique(qx))
@@ -116,7 +120,7 @@ def do_the_job(file_name):
     qx_2d = qx.reshape(shape_qx, shape_qy)
     qy_2d = qy.reshape(shape_qx, shape_qy)
     i_2d = i.reshape(shape_qx, shape_qy)
-
+    
     # Plot QxQy
     fig = plt.figure()
     ax1 = fig.add_subplot(321)
@@ -140,8 +144,8 @@ def do_the_job(file_name):
 
     # Angle
     ax3 = fig.add_subplot(323)
-    x, y, angle = angle_calculation(qx, qy, i)
-    im3 = ax3.scatter(x, y, c=angle, s=50, edgecolor='', marker='s')
+    angle = angle_calculation(qx, qy)
+    im3 = ax3.scatter(qx, qy, c=angle, s=50, edgecolor='', marker='s')
     plt.colorbar(im3, ax=ax3)
     ax3.set_ylabel('Angle')
     ax3.set_xlabel("Qx")
@@ -149,7 +153,8 @@ def do_the_job(file_name):
 
     # Wedge
     ax4 = fig.add_subplot(324)
-    x, y, z = wedge_calculation(qx, qy, i, angle, phi_0=30, phi_apperture=45)
+    x, y, z, err_z = wedge_calculation(qx, qy, i, err_i, angle,
+                                       phi_0=30, phi_aperture=45)
     im = ax4.scatter(x, y, c=z, s=50, edgecolor='', marker='s')
     plt.colorbar(im, ax=ax4)
     ax4.set_ylabel('Wedge')
@@ -158,12 +163,12 @@ def do_the_job(file_name):
 
     # Wedge integration
     ax5 = fig.add_subplot(325)
-    x1d, y1d = wedge_integration(x, y, z)
-    ax5.plot(x1d, y1d)
+    x1d, y1d, err1d = wedge_integration(x, y, z, err_z)
+    # ax5.plot(x1d, y1d)
+    ax5.errorbar(x1d, y1d, yerr=err1d, fmt='b', ecolor='g')
     ax5.set_ylabel('Wedge Integration')
     ax5.set_xlabel("Q")
     ax5.set_ylabel("I(q)")
-    
 
     plt.show()
     # plt.show(block=False)
